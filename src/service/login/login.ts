40bc144562af1a980c819e9de7d1face93f972ad
AmberQu <1037313052@qq.com>
2021-11-02 15:45:39 +0800

    登录请求++请求用户信息

---
+++ src/service/login/login.ts
@@ -0,0 +1,34 @@
+import wRequest from '../index'
+
+import { IAccount } from './type'
+
+enum LoginAPI {
+    AccountLogin = '/login',
+    LoginUserInfo = '/users/', // users/id
+    UserMenus = '/role/' // role/id/menu
+}
+
+//登录请求
+export function accountLoginRequest(account: IAccount) {
+    return wRequest.post({
+        url: LoginAPI.AccountLogin,
+        data: {
+            "name": "codertest",
+            "password": "123456"
+        },
+    })
+}
+
+//请求用户信息
+export function requestUserInfoById(id: number) {
+    return wRequest.get({
+        url: LoginAPI.LoginUserInfo + id,
+    })
+}
+
+//请求菜单
+export function requestUserMenusByRoleId(id: number) {
+    return wRequest.get({
+        url: LoginAPI.UserMenus + id + '/menu',
+    })
+}
