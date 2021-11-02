2522bc680186530a97f713ef50c2e71bc7208e87
AmberQu <1037313052@qq.com>
2021-11-02 17:07:28 +0800

    搭建main页面

---
+++ src/components/nav-menu/src/nav-menu.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="nav-menu">
+       <div class="flex nav-logo-wrap">
+           <img src="~assets/img/logo.svg" alt="" class="nav-logo">
+           <span>vue3+ts</span>
+       </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from "vue"
+
+export default defineComponent({
+   components: {
+      
+   }
+})
+</script>
+
+<style lang="less" scoped>
+.nav-logo-wrap {
+    margin: 14px 0 14px 20px;
+    color: #fff;
+    .nav-logo {
+        width: 20px;
+        height: 20px;
+        margin-right: 10px;
+    }
+}
+    
+</style>
+
---
+++ src/views/main/main.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <el-container class="menu-contaniner">
+            <el-aside width="210px" class="menu-aside">
+                <NavMenu></NavMenu>
+            </el-aside>
+            <el-container>
+                <el-header class="menu-header">Header</el-header>
+                <el-main class="menu-main">Main</el-main>
+            </el-container>
+        </el-container>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from "vue"
+import NavMenu from '@/components/nav-menu'
+
+export default defineComponent({
+   components: {
+       NavMenu
+   }
+})
+</script>
+
+<style lang="less" scoped>
+.menu-contaniner {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    .menu-aside {
+        background-color: #001529;
+    }
+    .menu-header {
+        height: 48px;
+        line-height: 48px;
+    }
+    .menu-main {
+        background-color: #f0f2f5;
+    }
+}
+</style>
+
